c84bbb6703452430480de25239eaa4c976841963
Matt Jennings <matt@mattjennings.net>
2024-04-09 16:36:07 -0700

    done with eslint and prettier setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,21 @@
 module.exports = {
-  root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  extends: ["airbnb", "plugin:jsx-a11y/recommended", "prettier"],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh", "jsx-a11y"],
   rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true }
     ],
-  },
-}
+    semi: "error",
+    quotes: ["error", "double"],
+    "jsx-quotes": ["error", "prefer-double"],
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "import/no-absolute-path": "off"
+  }
+};
